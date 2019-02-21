94420b977f2ef1bebec194848d1aec72b892fcfc
AlexGruebel <alexander-gruembel@onine.de>
2019-02-21 10:33:08 +0100

    add BaseController as superclass for controllers

---
+++ RESTAPIusermanagementproject/src/main/java/com/ap/usermanagementproject/controller/BaseController.java
@@ -0,0 +1,37 @@
+package com.ap.usermanagementproject.controller;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public abstract class BaseController<T, R extends JpaRepository<T, Integer>> extends BaseGetController<T, R> {
+
+    @GetMapping(value = "/{id}")
+    public T get(@PathVariable int id) {
+        Optional<T> findById = super.getRepository().findById(id);
+        return (T) findById;
+    }
+
+    @PostMapping
+    public T post(@RequestBody T entity) {
+        super.getRepository().save(entity);
+        return entity;
+    }
+    
+    @PutMapping(value="/{id}")
+    public T put(@PathVariable int id, @RequestBody T entity) {
+        super.getRepository().save(entity);
+        return entity;
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable int id){
+        super.getRepository().deleteById(id);
+    }
+} 
--- RESTAPIusermanagementproject/src/main/java/com/ap/usermanagementproject/controller/UserDetailController.java
+++ RESTAPIusermanagementproject/src/main/java/com/ap/usermanagementproject/controller/UserDetailController.java
@@ -1,53 +1,11 @@
 package com.ap.usermanagementproject.controller;
 
-import com.ap.usermanagementproject.entities.ResponseBodyMultipleItems;
 import com.ap.usermanagementproject.entities.UserDetail;
 import com.ap.usermanagementproject.repositories.UserDetailRepository;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-public class UserDetailController{
-
-    private UserDetailRepository userDetailRepository;
-
-    public UserDetailController(UserDetailRepository userDetailRepository){
-        this.userDetailRepository = userDetailRepository;
-    }
-
-
-    @GetMapping(value="/userdetail")
-    public ResponseBodyMultipleItems<UserDetail> getAllUserDetails(@RequestParam(required = false, defaultValue = "1") Integer page
-                                                                  ,@RequestParam(required = false, defaultValue = "100") Short limit) 
-    {
-        ResponseBodyMultipleItems<UserDetail> body = new ResponseBodyMultipleItems<UserDetail>(page, limit);
-        body.fillBodyWithRepository(userDetailRepository);
-        return body;
-    }
-    
-    @PostMapping(value="/userdetail")
-    public UserDetail postUser(@RequestBody UserDetail entity) {
-        this.userDetailRepository.save(entity);
-        return entity;
-    }
-    
-    @PutMapping(value="/userdetail/{id}")
-    public UserDetail putUser(@PathVariable int id, @RequestBody UserDetail entity) {
-        this.userDetailRepository.save(entity);
-        return entity;
-    }
-
-    @DeleteMapping
-    public void deleteUser(@PathVariable int id){
-        this.userDetailRepository.deleteById(id);
-    }
-}
+public class UserDetailController extends BaseController<UserDetail, UserDetailRepository>{}
