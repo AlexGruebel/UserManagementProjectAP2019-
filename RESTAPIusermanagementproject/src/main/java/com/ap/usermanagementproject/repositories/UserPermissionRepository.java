000bc9043ce9ca0fd2401940826c3c758c88c446
AlexGruebel <alexander-gruembel@onine.de>
2019-02-26 08:34:27 +0100

    add method findByUserid and remove old unused method

--- RESTAPIusermanagementproject/src/main/java/com/ap/usermanagementproject/repositories/UserPermissionRepository.java
+++ RESTAPIusermanagementproject/src/main/java/com/ap/usermanagementproject/repositories/UserPermissionRepository.java
@@ -1,12 +1,13 @@
 package com.ap.usermanagementproject.repositories;
 
-import com.ap.usermanagementproject.entities.UserPermission;
+import java.util.Set;
+
+import com.ap.usermanagementproject.entities.UserAuthorityEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface UserPermissionRepository extends JpaRepository<UserPermission, Integer>{
-     //Query value becouse I dont found a way to build a "? like column" with the method name
-     @Query(value = "select ifnull((select 'true' from user_permissions c where c.user_id= ?1 and ?2 like c.permission limit 1),'false')", nativeQuery=true)
-     boolean existsByUserIdAndPermissionLike(int userid, String Permission);
+public interface UserPermissionRepository extends JpaRepository<UserAuthorityEntity, Integer>{
+     
+     Set<UserAuthorityEntity> findByUserid(int userid);
 }
